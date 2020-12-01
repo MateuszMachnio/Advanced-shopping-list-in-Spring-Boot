1793aef288ede9b88ecc18da7184f6f9343e73c5
root <mateusz.machnio.9@gmail.com>
2020-12-01 23:44:43 +0100

    adding recipe to plan after saving the plan

--- src/main/java/pl/machnio/shoppingList/controller/LoggedUserPlanController.java
+++ src/main/java/pl/machnio/shoppingList/controller/LoggedUserPlanController.java
@@ -4,14 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import pl.machnio.shoppingList.entity.Plan;
-import pl.machnio.shoppingList.entity.User;
-import pl.machnio.shoppingList.service.PlanService;
-import pl.machnio.shoppingList.service.UserService;
+import pl.machnio.shoppingList.entity.*;
+import pl.machnio.shoppingList.service.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequestMapping("/logged-user/plan")
@@ -19,11 +19,35 @@ public class LoggedUserPlanController {
 
     private final PlanService planService;
     private final UserService userService;
+    private final RecipeService recipeService;
+    private final MealNameService mealNameService;
+    private final DayOfTheWeekService dayOfTheWeekService;
+    private final PlanScheduleService planScheduleService;
 
-    public LoggedUserPlanController(PlanService planService, UserService userService) {
+    public LoggedUserPlanController(PlanService planService, UserService userService, RecipeService recipeService, MealNameService mealNameService, DayOfTheWeekService dayOfTheWeekService, PlanScheduleService planScheduleService) {
         this.planService = planService;
         this.userService = userService;
+        this.recipeService = recipeService;
+        this.mealNameService = mealNameService;
+        this.dayOfTheWeekService = dayOfTheWeekService;
+        this.planScheduleService = planScheduleService;
     }
+
+    @ModelAttribute("recipes")
+    public List<Recipe> getRecipes() {
+        return recipeService.findAllRecipes();
+    }
+
+    @ModelAttribute("mealNames")
+    public List<MealName> getMealNames() {
+        return mealNameService.findAllMealNames();
+    }
+
+    @ModelAttribute("daysOfTheWeek")
+    public List<DayOfTheWeek> getDaysOfTheWeek() {
+        return dayOfTheWeekService.findAllDaysOfTheWeek();
+    }
+
 
     @GetMapping("/add")
     public String addPlan(Model model) {
@@ -32,7 +56,7 @@ public class LoggedUserPlanController {
     }
 
     @PostMapping("/add")
-    public String addingPlan(@Valid Plan plan, BindingResult result) {
+    public String addingPlan(@Valid Plan plan, BindingResult result, Model model) {
         if (result.hasErrors()) {
             return "/logged-user/plan/add";
         }
@@ -40,7 +64,24 @@ public class LoggedUserPlanController {
         User currentUser = userService.getCurrentUserWithPlans();
         currentUser.addPlan(savedPlan);
         userService.updateUser(currentUser);
-        return "redirect:/logged-user/dashboard";
+        model.addAttribute("planSchedule", new PlanSchedule());
+        model.addAttribute("planId", savedPlan.getId());
+        return "/logged-user/plan/addRecipeToPlan";
+    }
+
+    @PostMapping("/add-recipe")
+    public String addRecipeToPlan(@Valid PlanSchedule planSchedule, BindingResult result) {
+        if (result.hasErrors()) {
+            return "/logged-user/plan/addRecipeToPlan";
+        }
+        planScheduleService.savePlanSchedule(planSchedule);
+        return "redirect:list";
+    }
+
+    @GetMapping("/list")
+    public String showPlans(Model model) {
+        model.addAttribute("plans", userService.getCurrentUserWithPlans().getPlans());
+        return "/logged-user/plan/list";
     }
 
 }
--- src/main/webapp/WEB-INF/views/logged-user/plan/addRecipeToPlan.jsp
+++ src/main/webapp/WEB-INF/views/logged-user/plan/addRecipeToPlan.jsp
@@ -1,16 +1,75 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: machn
-  Date: 01.12.2020
-  Time: 18:24
-  To change this template use File | Settings | File Templates.
---%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>$Title$</title>
+    <title>Dodawanie przepisu do planu</title>
+    <%@include file="../../../constantParts/head.jspf" %>
 </head>
 <body>
-$END$
+<div class="container">
+    <%@include file="../../../constantParts/loggedHeader.jspf"%>
+
+    <div id="content">
+        <div id="topOfContent" style="margin-top: 10px">
+        </div>
+
+        <div id="middleOfContent">
+            <%@include file="../../../constantParts/loggedSidebar.jsp"%>
+
+            <div id="text" style="text-align: center">
+                <h1>Dodawanie przepisu do planu</h1>
+
+                <form:form modelAttribute="planSchedule" method="post" action="/logged-user/plan/add-recipe">
+                    <table>
+                        <tr>
+                            <td><form:label path="dayOfTheWeek">Dzień tygodnia: </form:label></td>
+                            <td>
+                                <form:select path="dayOfTheWeek">
+                                    <form:option value="0" label="--select day--"/>
+                                    <form:options items="${daysOfTheWeek}" itemLabel="name" itemValue="id" />
+                                </form:select>
+                            </td>
+                            <td><form:errors path="dayOfTheWeek" cssClass="error"/></td>
+                        </tr>
+
+                        <tr>
+                            <td><form:label path="mealName">Nazwa posiłku: </form:label></td>
+                            <td>
+                                <form:select path="mealName">
+                                    <form:option value="0" label="--select meal--"/>
+                                    <form:options items="${mealNames}" itemLabel="name" itemValue="id" />
+                                </form:select>
+                            </td>
+                            <td><form:errors path="mealName" cssClass="error"/></td>
+                        </tr>
+
+                        <tr>
+                            <td><form:label path="recipe">Nazwa przepisu: </form:label></td>
+                            <td>
+                                <form:select path="recipe">
+                                    <form:option value="0" label="--select recipe--"/>
+                                    <form:options items="${recipes}" itemLabel="name" itemValue="id" />
+                                </form:select>
+                            </td>
+                            <td><form:errors path="recipe" cssClass="error"/></td>
+                        </tr>
+                        <form:hidden path="plan" value="${planId}"/>
+                    </table>
+                    <br />
+
+                    <input type="submit" value="Zapisz">
+                    <%--                    <form:button><a href="<c:url value="/logged_user/dashboard"/>">Gotowe</a></form:button>--%>
+                </form:form>
+
+            </div>
+        </div>
+        <!-- koniec środka zawartości -->
+        <div id="bottomOfContent">
+        </div>
+    </div>
+    <!--  koniec divContent  -->
+    <%@include file="../../../constantParts/footer.jspf"%>
+</div>
 </body>
 </html>
