10036380222cf9266e596e08e32c6350b2ca2258
root <mateusz.machnio.9@gmail.com>
2020-12-11 18:24:00 +0100

    created my own Converter for Ingredient

--- src/main/java/pl/machnio/shoppingList/converter/IngredientConverter.java
+++ src/main/java/pl/machnio/shoppingList/converter/IngredientConverter.java
@@ -1,4 +1,21 @@
 package pl.machnio.shoppingList.converter;
 
-public class IngredientConverter {
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+import pl.machnio.shoppingList.entity.Ingredient;
+import pl.machnio.shoppingList.service.IngredientService;
+
+@Component
+public class IngredientConverter implements Converter<String, Ingredient> {
+
+    private final IngredientService ingredientService;
+
+    public IngredientConverter(IngredientService ingredientService) {
+        this.ingredientService = ingredientService;
+    }
+
+    @Override
+    public Ingredient convert(String s) {
+        return ingredientService.findByName(s);
+    }
 }
